f051b9e0b1ad5cd65521c6aceaeaf6651d96b944
KARAN JOSHI <64606159+karanjoshi1206@users.noreply.github.com>
2021-01-12 22:22:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+  let scroll_pos = 0;
+  $(document).scroll(function () {
+    scroll_pos = $(this).scrollTop();
+    if (scroll_pos > 20) {
+      $(".navbar").css("background-color", "red");
+      $(".navbar").css("padding", "10px 0px");
+    } else {
+      $(".navbar").css("background", "none");
+    }
+  });
+});
+function toggle() {
+  let menu = document.querySelector(".menu")
+  let bar = document.querySelector(".menu-btn i")
+  menu.classList.toggle("active")
+  bar.classList.toggle("active")
+}
+
+$("#scroll").click(function () {
+  $([document.documentElement, document.body]).animate(
+    {
+      scrollTop: $("#home").offset().top,
+    }
+  );
+});
+
+
+
+var typed = new Typed(".typing", {
+  strings: ["Web Developer", "Student", "Coder", "Learner"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
+
+
+function preloader() {
+  const preloader = document.getElementById("preloader")
+  preloader.style.display = "none"
+}
